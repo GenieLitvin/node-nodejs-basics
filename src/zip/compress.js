fb5e2aaff46cf5dcc4281447835d8b2f5fedd392
Yauheniya Litvin <genie.litvin@gmail.com>
2024-01-25 16:04:28 +0100

    zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+  createReadStream,
+  createWriteStream,
+} from 'node:fs';
+import { promisify }from 'node:util';
+const pipe = promisify(pipeline);
+
+
+const sourcePath = './src/zip/files/fileToCompress.txt';
+const distPath = './src/zip/files/archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(distPath);
+    await pipe(source, gzip, destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+  createReadStream,createWriteStream
+} from 'node:fs';
+import { promisify }from 'node:util';
+
+const distPath = './src/zip/files/fileToCompress.txt';
+const sourcePath = './src/zip/files/archive.gz';
+
 const decompress = async () => {
-    // Write your code here 
+    const source = createReadStream(sourcePath);
+    const writeStream = createWriteStream(distPath);
+    const unzip = createUnzip();
+  
+    source.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
