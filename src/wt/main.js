ad5d1f868fa5ca1be97c1d913d720f007bb7fba5
Yauheniya Litvin <genie.litvin@gmail.com>
2024-01-26 11:43:29 +0100

    wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,19 @@
+import { Worker} from 'node:worker_threads';
+import os from 'node:os';
+const cpuCores = os.cpus().length;
+ 
+const result = [];
+let finished =0;
+
 const performCalculations = async () => {
-    // Write your code here
+
+  for (let i = 0; i < cpuCores; i++) {
+    const worker = new Worker('./src/wt/worker.js',{ workerData: 10+ i});  
+    worker.on('message', data => {result[i]=data; } );
+    worker.on("exit", code => { finished++; if(finished==4) console.log(result)} );
+  } 
+
 };
+
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
+import { workerData, threadId, parentPort } from "node:worker_threads";
+
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    try{
+      const value = nthFibonacci(workerData);
+      parentPort.postMessage({ data : value,  status: 'resolved'});
+    }catch(err){
+      parentPort.postMessage({ data : null,  status: 'error'});
+    }
 };
 
 sendResult();
+
