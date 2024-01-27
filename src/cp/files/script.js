f90cf7cde4b78cb674af24d30ed6f2cd49a19e20
Yauheniya Litvin <genie.litvin@gmail.com>
2024-01-27 20:15:20 +0100

    fixed cp

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -9,4 +9,4 @@ const echoInput = (chunk) => {
     process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
 };
 
-process.stdin.on('data', close);
+process.stdin.on('data', echoInput);
